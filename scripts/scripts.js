bfde25cc9c10b4fc692276d7044855e6674fa5f0
Nacho <nachodl.queso@gmail.com>
2020-08-23 20:13:14 +0200

    Added the animation and the input/output, finished by the moment

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,17 @@
+let buttons = document.querySelectorAll('button');
+let playerPic = document.getElementById('player_choice');
+let computerPic = document.getElementById('computer_choice');
+let displayPlayerScore = document.getElementById('player_score');
+let displayComputerScore = document.getElementById('computer_score');
+let displayMessage = document.getElementById('message');
+
+buttons.forEach((button) => {
+    let playerSelection = button.id;
+    button.addEventListener('click', (e) => playRound(playerSelection));
+});
+
 function computerPlay() {
-    let play = Math.random() * 3
+    let play = Math.random() * 3;
     if (play >= 1) {
         if (play >= 2) {
             return "Scissors"
@@ -11,44 +23,69 @@ function computerPlay() {
     }
 }
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toString()
-    computerSelection = computerSelection.toString()
+function playRound(playerSelection) {
+    playerSelection = playerSelection.toString().toLowerCase();
+    let computerSelection = computerPlay().toString().toLowerCase();
+
+    let playerWin = false;
+    let computerWin = false;
+
+    function noWinner() {
+        playerWin = false;
+        computerWin = false;
+    }
+
+    function playerWins() {
+        playerWin = true;
+        computerWin = false;
+    }
+
+    function computerWins() {
+        playerWin = false;
+        computerWin = true;
+    }
+
     if (playerSelection.toLowerCase() 
         == computerSelection.toLowerCase()) {
-            return "Draw! You both chosed " + playerSelection + "!"
-    } else {
-        switch (playerSelection.toLowerCase()) {
-            case "rock":
-                if (computerSelection.toLowerCase() == "scissors") {
-                    return "You won! Rock beats scissors!"
-                } else {
-                    return "You lose! Paper beats rock..."
-                }
-                break
-            case "paper":
-                if (computerSelection.toLowerCase() == "rock") {
-                    return "You won! Paper beats rock!"
-                } else {
-                    return "You lose! Scissors beats paper..."
-                }
-                break
-            case "scissors":
-                if (computerSelection.toLowerCase() == "paper") {
-                    return "You won! Scissors beats paper!"
-                } else {
-                    return "You lose! Rock beats scissors..."
-                }
-                break
-            default:
-                return "error"
-        }
+            noWinner();
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'paper') ||
+		(playerSelection === 'paper' && computerSelection === 'scissors') ||
+		(playerSelection === 'scissors' && computerSelection === 'rock')
+    ) {
+        computerWins();
+    } else if (
+		(playerSelection === 'rock' && computerSelection === 'scissors') ||
+		(playerSelection === 'paper' && computerSelection === 'rock') ||
+		(playerSelection === 'scissors' && computerSelection === 'paper')
+	) {
+		playerWins();
     }
+
+    setPics(playerSelection.toLowerCase(), computerSelection.toLowerCase());
+    game(playerWin, computerWin, playerSelection, computerSelection);
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerPlay = prompt("Choose your play: rock, paper or scissors", "error")
-        console.log(playRound(playerPlay, computerPlay()))
+function setPics(playerSelection, computerSelection) {
+    playerPic.src = `images/${playerSelection}.png`;
+	computerPic.src = `images/${computerSelection}_fliped.png`;
+}
+
+let playerScore = 0;
+let computerScore = 0;
+let isGameOver = false;
+
+function game(playerWin, computerWin, playerSelection, computerSelection) {
+    if (playerWin) {
+        playerScore++;
+        displayPlayerScore.innerHTML = `${playerScore}`;
+        displayMessage.innerHTML = `You have beaten him!`;
+    } else if (computerWin) {
+        computerScore++;
+        displayComputerScore.innerHTML = `${computerScore}`;
+        displayMessage.innerHTML = `He has beaten you!`;
+    }
+    else {
+        displayMessage.innerHTML = `It's a draw`;
     }
 }
